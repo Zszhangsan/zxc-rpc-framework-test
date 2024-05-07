f7f2fc6642e1e95fedfb6865d38e2ba26812e016
zxc <banshanzhitu.@163.com>
2024-05-07 22:10:30 +0800

    remote

---
+++ zxc-rpc-remote/src/main/java/zxc/rpc/zxcrpcremote/remoting/dto/RPCRequest.java
@@ -0,0 +1,8 @@
+package zxc.rpc.zxcrpcremote.remoting.dto;
+
+import java.io.Serializable;
+
+public class RPCRequest implements Serializable {
+    private static final long serialVersionUID = -4217987815084902760L;
+    
+}
---
+++ zxc-rpc-remote/src/test/java/zxc/rpc/zxcrpcremote/ZxcRpcRemoteApplicationTests.java
@@ -0,0 +1,13 @@
+package zxc.rpc.zxcrpcremote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZxcRpcRemoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
